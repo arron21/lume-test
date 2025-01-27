cdfa515ee906bde9b728ef78511f3975b260fe96
arron <arronmccrory@gmail.com>
2025-01-27 09:37:20 -0500

    testing index doc

--- _cms.ts
+++ _cms.ts
@@ -2,4 +2,16 @@ import lumeCMS from "lume/cms/mod.ts";
 
 const cms = lumeCMS();
 
+cms.document({
+    name: "landing-page",
+    label: "This is your homepage",
+    description: "Edit the content of the home page",
+    store: "src:index.yml",
+    fields: [
+      "title: text",
+      "subtitle: text",
+      "content: markdown",
+    ],
+});
+
 export default cms;
