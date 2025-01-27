03ec85341cbaa16a8858e390e02af8d57ccef33a
arron <arronmccrory@gmail.com>
2025-01-27 09:39:33 -0500

    updated auth

--- _cms.ts
+++ _cms.ts
@@ -1,6 +1,24 @@
 import lumeCMS from "lume/cms/mod.ts";
 
-const cms = lumeCMS();
+const cms = lumeCMS(
+    {
+        site: {
+            name: "My blog CMS",
+            description: "Here I can edit the content of my blog",
+            url: "https://myblog.com",
+            body: `
+            <p>Long text, for instructions or other content that you want to make it visible in the homepage</p>
+            `,
+          },
+          auth: {
+            method: "basic",
+            users: {
+              user1: "password1",
+              user2: "password2",
+            },
+          },
+    }
+);
 
 cms.document({
     name: "landing-page",
